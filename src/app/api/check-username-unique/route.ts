0b264fd53f624164f97e330c86fea4cee6ca72c6
Rohan Kulkarni <rohank2502@gmail.com>
2024-06-15 15:44:06 +0530

    Routes : Added Check Unique Username route

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,76 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+  // localhost:3000/api/check-username-unique?username=rohan
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    // validate with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors.length > 0
+              ? usernameErrors.join(",")
+              : "Invalid query parameters",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "username is already taken",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: "username is available",
+        },
+        {
+          status: 200,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in checking username",
+      },
+      { status: 500 }
+    );
+  }
+}
